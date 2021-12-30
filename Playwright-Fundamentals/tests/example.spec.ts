d7a27c2a5a6b21d549d6c2f99f2a61143e4531bd
Martin <marb61a@gmail.com>
2021-12-30 16:43:44 +0000

    Updated test spec file with tests

--- Playwright-Fundamentals/tests/example.spec.ts
+++ Playwright-Fundamentals/tests/example.spec.ts
@@ -9,3 +9,25 @@ test("Basic test", async({page}) => {
 
     await expect(pageTitle).toContainText("Example Domain")
 })
+
+test("Clicking on elements", async({page}) => {
+    // Use the zero webappsecurity page for the example
+    await page.goto("http://zero.webappsecurity.com/index.html")
+
+    // Click on the signin button
+    await page.click("#signin_button")
+
+    // Select the sign in button using Playwright which can selec element by text
+    await page.click("text=Sign in")
+
+    // Create error message variable which will be used in assertion
+    const errorMessage = await page.locator(".alert-error")
+
+    // Check the error message is ok
+    await expect(errorMessage).toContainText("Login and/or password are wrong")
+})
+
+test ("selectors", async ({page}) => {
+    // Will be only demow examples without specific site
+    
+})
