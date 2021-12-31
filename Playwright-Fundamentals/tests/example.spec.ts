d3b2ae8f07632d52fd594fa637be811255f267f7
Martin <marb61a@gmail.com>
2021-12-31 16:34:44 +0000

    Updated example spec file with demo of common selectors

--- Playwright-Fundamentals/tests/example.spec.ts
+++ Playwright-Fundamentals/tests/example.spec.ts
@@ -27,7 +27,28 @@ test("Clicking on elements", async({page}) => {
     await expect(errorMessage).toContainText("Login and/or password are wrong")
 })
 
-test ("selectors", async ({page}) => {
-    // Will be only demow examples without specific site
+// test ("selectors", async ({page}) => {
+//     // Will be only demoing examples without specific site
+//     // Text will always just be the value of the text exactly !!!
+//     await page.click('text=some text')
+
+//     // CSS Selectors can be various things such as class or id
+//     await page.click('button')
+//     // ID
+//     await page.click('#button')
+//     //Class
+//     await page.click('.button')
+
+//     // Only visible CSS selector : click on button only if visible
+//     await page.click('button:visible')
+
+//     // Combinations this can be any combination
+//     await page.click('#button .first')
+
+//     // XPath - use any valid XPath
+//     await page.click('//button')
+// })
+
+test("Work with inputs", async({page}) => {
     
 })
