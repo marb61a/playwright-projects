aa0f44444a2836f8ecbedb5cc35e1cd638c7650c
Martin <marb61a@gmail.com>
2022-01-04 16:46:54 +0000

    Added annotations examples to fundamentals project folder

--- Playwright-Fundamentals/tests/example.spec.ts
+++ Playwright-Fundamentals/tests/example.spec.ts
@@ -27,74 +27,76 @@ test("Clicking on elements", async({page}) => {
     await expect(errorMessage).toContainText("Login and/or password are wrong")
 })
 
-// test ("selectors", async ({page}) => {
-//     // Will be only demoing examples without specific site
-//     // Text will always just be the value of the text exactly !!!
-//     await page.click('text=some text')
+test.skip ("selectors", async ({page}) => {
+    // Will be only demoing examples without specific site
+    // Text will always just be the value of the text exactly !!!
+    await page.click('text=some text')
 
-//     // CSS Selectors can be various things such as class or id
-//     await page.click('button')
-//     // ID
-//     await page.click('#button')
-//     //Class
-//     await page.click('.button')
+    // CSS Selectors can be various things such as class or id
+    await page.click('button')
+    // ID
+    await page.click('#button')
+    //Class
+    await page.click('.button')
 
-//     // Only visible CSS selector : click on button only if visible
-//     await page.click('button:visible')
+    // Only visible CSS selector : click on button only if visible
+    await page.click('button:visible')
 
-//     // Combinations this can be any combination
-//     await page.click('#button .first')
+    // Combinations this can be any combination
+    await page.click('#button .first')
 
-//     // XPath - use any valid XPath
-//     await page.click('//button')
-// })
-
-test("Work with inputs", async({page}) => {
-    // Use the zero webappsecurity page for the example
-    await page.goto("http://zero.webappsecurity.com/index.html")
-
-    // Click on the signin button
-    await page.click("#signin_button")
-
-    // Start filling in form with login field
-    await page.type('#user_login', 'some username')
-
-    // Password field
-    await page.type('#user_password', 'some password')
-
-    // Select the sign in button using Playwright which can selec element by text
-    await page.click("text=Sign in")
-
-    // Create error message variable which will be used in assertion
-    const errorMessage = await page.locator(".alert-error")
-
-    // Check the error message is ok
-    await expect(errorMessage).toContainText("Login and/or password are wrong")
-
+    // XPath - use any valid XPath
+    await page.click('//button')
 })
 
-test("Testing assertions", async({page}) => {
-    // Using example.com as site for testing
-    await page.goto("https://www.example.com")
-
-    // Assert URL
-    await expect(page).toHaveURL("https://www.example.com")
-
-    // Assert title
-    await expect(page).toHaveTitle("Example Domain")
-
-    // Assert element visibility
-    const element = await page.locator('h1')
-    await expect(element).toBeVisible()
-
-    // Assert text value
-    await expect(element).toHaveText("Example Domain")
-
-    // Assert element count
-    await expect(element).toHaveCount(1)
-
-    // Assert element not visible
-    const nonExistingElement = await page.locator('h5')
-    await expect(nonExistingElement).not.toBeVisible()
+test.describe("My first test suite", () => {
+    test("Work with inputs", async({page}) => {
+        // Use the zero webappsecurity page for the example
+        await page.goto("http://zero.webappsecurity.com/index.html")
     
+        // Click on the signin button
+        await page.click("#signin_button")
+    
+        // Start filling in form with login field
+        await page.type('#user_login', 'some username')
+    
+        // Password field
+        await page.type('#user_password', 'some password')
+    
+        // Select the sign in button using Playwright which can selec element by text
+        await page.click("text=Sign in")
+    
+        // Create error message variable which will be used in assertion
+        const errorMessage = await page.locator(".alert-error")
+    
+        // Check the error message is ok
+        await expect(errorMessage).toContainText("Login and/or password are wrong")
+    
+    })
+    
+    test("Testing assertions", async({page}) => {
+        // Using example.com as site for testing
+        await page.goto("https://www.example.com")
+    
+        // Assert URL
+        await expect(page).toHaveURL("https://www.example.com")
+    
+        // Assert title
+        await expect(page).toHaveTitle("Example Domain")
+    
+        // Assert element visibility
+        const element = await page.locator('h1')
+        await expect(element).toBeVisible()
+    
+        // Assert text value
+        await expect(element).toHaveText("Example Domain")
+    
+        // Assert element count
+        await expect(element).toHaveCount(1)
+    
+        // Assert element not visible
+        const nonExistingElement = await page.locator('h5')
+        await expect(nonExistingElement).not.toBeVisible()
+    
+    })
 })
