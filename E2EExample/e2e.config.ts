7fb2aeec71ac119e834c143640cb2c9c954d0d55
Martin <marb61a@gmail.com>
2022-01-05 16:48:17 +0000

    Updated config file in e2e project folder and updated notes

--- E2EExample/e2e.config.ts
+++ E2EExample/e2e.config.ts
@@ -5,12 +5,27 @@ const config: PlaywrightTestConfig = {
     retries: 0,
     use: {
         headless: true,
-
+        viewport: {
+            width: 1280,
+            height: 720
+        },
+        actionTimeout: 15000,
+        ignoreHTTPSErrors: true,
+        video: 'off',
+        screenshot: 'off'
     },
     projects: [
         {
             name: 'Chromium',
             use: {browserName: 'chromium'}
+        },
+        {
+            name: 'Firefox',
+            use: {browserName: 'firefox'}
+        },
+        {
+            name: 'Webkit',
+            use: {browserName: 'webkit'}
         }
     ]
 }
