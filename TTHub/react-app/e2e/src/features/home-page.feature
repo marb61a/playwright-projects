c8ab008d515f1709e540d399b50a3aeff6ac5295
Martin <marb61a@gmail.com>
2022-02-21 18:34:31 +0000

    Updated definition and features files

--- TTHub/react-app/e2e/src/features/home-page.feature
+++ TTHub/react-app/e2e/src/features/home-page.feature
@@ -5,5 +5,5 @@ Feature: As a user I expect to be able to navigate to the home page
   @regression
   Scenario: As a user I expect to be able to see contacts
     Given I am on the "home" page
-    Then the contacts header should contain the text Contacts
+    Then the "contacts header" should contain the text "Contacts"
 
--- TTHub/react-app/e2e/src/step-definitions/assertions/verify-element-visibility.ts
+++ TTHub/react-app/e2e/src/step-definitions/assertions/verify-element-visibility.ts
@@ -2,14 +2,14 @@ import { Then } from '@cucumber/cucumber'
 import { expect } from '@playwright/test'
 
 Then(
-    /^the contacts header should contain the text Contacts$/,
-    async function() {
+    /^the "([^"]*)" should contain the text "(.*)"$/,
+    async function(elementKey: string, expectedElementText: string) {
 
-        console.log("the contacts header should contain the text Contacts")
+        console.log(`the ${elementKey} should contain the text ${expectedElementText}`)
 
         const content = await global.page.textContent("[data-id='contacts']")
 
-        expect(content).toBe('Contacts')
+        expect(content).toBe(expectedElementText)
 
     }
 )
