986cee612bb1ca4c9765ea4842471b1189929577
Martin <marb61a@gmail.com>
2022-02-22 18:22:52 +0000

    Updated world.ts file

--- TTHub/react-app/e2e/src/step-definitions/setup/world.ts
+++ TTHub/react-app/e2e/src/step-definitions/setup/world.ts
@@ -18,7 +18,29 @@ export class ScenarioWorld extends World {
         super(options)
     }
 
-    screen:Screen
+    // Exclamation mark makes optional that this is set before being referenced
+    screen!: Screen
 
-    
-}
+    async init(contextOptions?: BrowserContextOptions): Promise<Screen> {
+        await this.screen?.page?.close()
+        await this.screen?.context?.close()
+        await this.screen?.browser?.close()
+
+        // This goes to a custom function that will determine a new browser
+        // which will be based on an env variable that has been set
+        const browser = await this.newBrowser()
+        const context = await browser.newContext(contextOptions)
+        const page = await context.newPage();
+
+        this.screen = {browser, context, page}
+
+        return this.screen
+    }
+
+    private newBrowser = async(): Promise<Browser> => {
+        const automationBrowsers = ['chromium', 'firefox', 'webkit']
+
+        type AutomationBrowser = typeof automationBrowsers[number]
+        const automationBrowser = env('UI_AUTOMATION_BROWSER') as AutomationBrowser
+    }
+ }
