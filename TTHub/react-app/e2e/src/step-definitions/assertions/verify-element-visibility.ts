2c1c61c1d43705572de6b374ca93acb85c792e4e
Martin <marb61a@gmail.com>
2022-02-25 18:03:20 +0000

    Updated assertions to remove use of global context

--- TTHub/react-app/e2e/src/step-definitions/assertions/verify-element-visibility.ts
+++ TTHub/react-app/e2e/src/step-definitions/assertions/verify-element-visibility.ts
@@ -4,10 +4,13 @@ import { expect } from '@playwright/test'
 Then(
     /^the "([^"]*)" should contain the text "(.*)"$/,
     async function(elementKey: string, expectedElementText: string) {
+        const {
+            screen: {page}
+        } = this
 
         console.log(`the ${elementKey} should contain the text ${expectedElementText}`)
 
-        const content = await global.page.textContent("[data-id='contacts']")
+        const content = await page.textContent("[data-id='contacts']")
 
         expect(content).toBe(expectedElementText)
 
@@ -17,9 +20,13 @@ Then(
 Then(
     /^the "([^"]*)" should be displayed/,
     async function (elementKey: string) {
+        const {
+            screen: {page}
+        } = this
+
         console.log(`The ${elementKey} should be displayed`)
 
-        const locator = global.page.locator("[data-id='header-logo']")
+        const locator = page.locator("[data-id='header-logo']")
 
         await expect(locator).toBeVisible()
     }
