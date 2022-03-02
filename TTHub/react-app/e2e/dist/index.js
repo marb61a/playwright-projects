a4d6bed578eccc0d1924e74f7020240c50930586
Martin <marb61a@gmail.com>
2022-03-02 17:20:13 +0000

    Updated package.json

--- TTHub/react-app/e2e/dist/index.js
+++ TTHub/react-app/e2e/dist/index.js
@@ -4,12 +4,24 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 exports.smoke = exports.regression = exports.dev = void 0;
-// Holds arguments instead of using package.json
+
+var _dotenv = _interopRequireDefault(require("dotenv"));
+
+var _parseEnv = require("./env/parseEnv");
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+// Import file which holds environmental variable that need to be set
+_dotenv.default.config({
+  path: (0, _parseEnv.env)('COMMON_CONFIG_FILE')
+}); // Holds arguments instead of using package.json
+
+
 var common = "./src/features/**/*.feature                 --require-module ts-node/register  \n                --require ./src/step-definitions/**/**/*.ts                 -f json:./reports/reports.json                 --format progress-bar"; // Creates profiles for different test situations (Previously in package.json scripts)
 
 var dev = "".concat(common, " --tags '@dev'");
 exports.dev = dev;
 var smoke = "".concat(common, " --tags '@smoke'");
 exports.smoke = smoke;
-var regression = "".concat(common, " --tags @smoke");
+var regression = "".concat(common, " --tags @regression");
 exports.regression = regression;
--- TTHub/react-app/e2e/src/index.ts
+++ TTHub/react-app/e2e/src/index.ts
@@ -1,10 +1,8 @@
 // Import file which holds environmental variable that need to be set
 import dotenv from 'dotenv' 
-import {env} from './env/parseEnv'
+import { env } from './env/parseEnv'
 
-dotenv.config({
-    path: env('COMMON_CONFIG_FILE')
-})
+dotenv.config({ path: env('COMMON_CONFIG_FILE')})
 
 // Holds arguments instead of using package.json
 const common = `./src/features/**/*.feature \
@@ -16,7 +14,7 @@ const common = `./src/features/**/*.feature \
 // Creates profiles for different test situations (Previously in package.json scripts)
 const dev = `${common} --tags '@dev'`
 const smoke = `${common} --tags '@smoke'`
-const regression = `${common} --tags @smoke`
+const regression = `${common} --tags '@regression'`
 
 
-export { dev, smoke, regression}
+export { dev, smoke, regression }
