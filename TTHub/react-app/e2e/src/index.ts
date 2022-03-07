a3e6acfadfd7445ae1c69701789c86c1dbe7fbe1
Martin <marb61a@gmail.com>
2022-03-07 17:28:30 +0000

    Moved global types file to src/env folder and updated file. Updated parseEnv file with getJsonFrom file function

--- TTHub/react-app/e2e/src/index.ts
+++ TTHub/react-app/e2e/src/index.ts
@@ -1,8 +1,22 @@
 // Import file which holds environmental variable that need to be set
 import dotenv from 'dotenv' 
 import { env } from './env/parseEnv'
+import {
+    GlobalConfig,
+    HostsConfig,
+    PagesConfig
+} from './env/global'
 
 dotenv.config({ path: env('COMMON_CONFIG_FILE')})
+
+// Retrives JSON mappings
+const hostsConfig: HostsConfig = getJsonFromFile(env('HOSTS_URL_PATH')) 
+const pagesConfig: PagesConfig = getJsonFromFile(env('PAGE_URLS_PATH')) 
+
+const worldParameters: GlobalConfig = {
+    hostsConfig,
+    pagesConfig
+}
 
 // Holds arguments instead of using package.json
 const common = `./src/features/**/*.feature \
