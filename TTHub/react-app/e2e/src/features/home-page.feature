29ce7ce988fc81c2c0eba1ce614861e60c75f1b2
Martin <marb61a@gmail.com>
2022-03-15 17:04:03 +0000

    Updated home page and radio buttons features to use negation and updated verify element value to allow for negative values

--- TTHub/react-app/e2e/src/features/home-page.feature
+++ TTHub/react-app/e2e/src/features/home-page.feature
@@ -7,3 +7,8 @@ Feature: As a user I expect to be able to navigate to the home page
     And the "header logo" should be displayed
     Then the "contacts header" should contain the text "Contacts"
 
+  Scenario: As a user I do not expect to see a contact that does not exist
+    Given I am on the "home" page
+    And I fill in the "search" input with "Weird Name"
+    Then the "contact" should not be displayed
+
--- TTHub/react-app/e2e/src/step-definitions/assertions/verify-element-value.ts
+++ TTHub/react-app/e2e/src/step-definitions/assertions/verify-element-value.ts
@@ -6,21 +6,22 @@ import {ScenarioWorld} from '../setup/world'
 import { waitFor } from '../../support/wait-for-behaviour'
 
 Then(
-    /^the "([^"]*)" should contain the text "(.*)"$/,
-    async function(this: ScenarioWorld, elementKey: ElementKey, expectedElementText: string) {
+    /^the "([^"]*)" should( not)? contain the text "(.*)"$/,
+    async function(this: ScenarioWorld, elementKey: ElementKey, negate: boolean, expectedElementText: string) {
         const {
             screen: {page},
             globalConfig
         } = this
 
-        // console.log(`the ${elementKey} should contain the text ${expectedElementText}`)
+        console.log(`the ${elementKey} should ${negate?'not':''} contain the text ${expectedElementText}`)
 
         const elementIdentifier = getElementLocator(page, elementKey, globalConfig)
 
         // Expect from playwright does not work well with custom waitFor functionality so is replaced
         await waitFor(async() => {
             const elementText = (await page.textContent(elementIdentifier))
-            return elementText?.includes(expectedElementText)
+            // Return will verify whether true or false
+            return elementText?.includes(expectedElementText) === !negate
         })
 
     }
