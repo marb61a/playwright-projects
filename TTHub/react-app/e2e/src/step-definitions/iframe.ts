7c13b75b0bc0921e791b7a5582ea2c7e4ddac4bb
Martin <marb61a@gmail.com>
2022-03-17 17:02:22 +0000

    Updated iframe.ts step definition file with function to get iframe

--- TTHub/react-app/e2e/src/step-definitions/iframe.ts
+++ TTHub/react-app/e2e/src/step-definitions/iframe.ts
@@ -7,7 +7,29 @@ import { waitFor } from '../support/wait-for-behaviour'
 
 Then(
     /^I fill in the "([^"]*)" input on the "([^"]*)" iframe with "([^"]*)"$/,
-    async function() {
+    async function(this: ScenarioWorld, elementKey: ElementKey, iFrameName: string, inputValue: string) {
+        const {
+            screen: { page},
+            globalConfig
+        } = this
 
+        console.log(`I fill in the ${elementKey} input on the ${iFrameName} with ${inputValue}`)
+        const elementIdentifier = getElementLocator(page, elementKey, globalConfig)
+        const iframeIdentifier = getElementLocator(page, iFrameName, globalConfig)
+        const elementIframe = await getIFrameElement(page, iframeIdentifier)
+
+        await waitFor(async() => {
+            const result = await page.waitForSelector(iframeIdentifier, {
+                state: 'visible'
+            })
+
+            if(result){
+                if(elementIframe){
+                    await inputValueOnIframe(elementIframe, elementIdentifier, inputValue)
+                }
+            }
+
+            return result
+        })
     }
 )
