1f4838b9f6595509ff73085cdad8ad9e6f1fa1e3
Martin <marb61a@gmail.com>
2022-03-18 18:16:24 +0000

    Updated page.ts then function

--- TTHub/react-app/e2e/src/step-definitions/page.ts
+++ TTHub/react-app/e2e/src/step-definitions/page.ts
@@ -8,7 +8,28 @@ import { inputValueOnPage } from '../support/html-behaviour'
 
 Then(
     /^I fill in the "([^"]*)" input on the "([0-9]+th|[0-9]+st|[0-9]+nd|[0-9]+rd)" (?:tab|window) with "([^"]*)"$/,
-    async function() {
-        
+    async function(this: ScenarioWorld, elementKey: ElementKey, elementPosition: string, input: string) {
+        const {
+            screen: { page, context },
+            globalConfig
+        } = this
+
+        console.log(`I fill in the ${elementKey} input on the ${elementPosition} tab|window with ${input}`)
+        const pageIndex = Number(elementPosition.match(/\d/g)?.join('')) - 1
+        const elementIdentifier = getElementLocator(page, elementKey, globalConfig)
+
+        await waitFor(async() => {
+            let pages = context.pages()
+
+            const result = await pages[pageIndex].waitForSelector(elementIdentifier, {
+                state: 'visible'
+            })
+
+            if(result) {
+                await inputValueOnPage(pages, pageIndex, elementIdentifier, input)
+            }
+
+            return result
+        })
     }
 )
