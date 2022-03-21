dee4cce700294d272f1c5f00a0a451cd06c0b51d
Martin <marb61a@gmail.com>
2022-03-21 17:55:16 +0000

    Updated verify-new-page assertion file with function for verifying whether label contains expected text

--- TTHub/react-app/e2e/src/features/playground/tabs.feature
+++ TTHub/react-app/e2e/src/features/playground/tabs.feature
@@ -11,3 +11,5 @@ Feature: As a user I can interact woth tabs
         And the "2nd" tab should contain the title "Contacts"
         And the "1st" tab should contain the title "Playground"
         And I fill in the "search" input on the "2nd" tab with "Sloane Juarez"
+        And the "contact" on the "2nd" tab should be displayed
+        And the "full name label" on the "2nd" tab should contain the text "Name:"
--- TTHub/react-app/e2e/src/step-definitions/assertions/verify-new-page.ts
+++ TTHub/react-app/e2e/src/step-definitions/assertions/verify-new-page.ts
@@ -32,6 +32,7 @@ Then(
     }
 )
 
+// Checking whether contact is displayed 
 Then(
     /^the "([^"]*)" on the "([0-9]+th|[0-9]+st|[0-9]+nd|[0-9]+rd)" (?:tab|window) should( not)? be displayed$/,
     async function(
@@ -54,6 +55,34 @@ Then(
             let pages = context.pages()
             const isElementVisible = (await pages[pageIndex].$(elementIdentifier)) != null
             return isElementVisible === !negate
+        })
+    }
+)
+
+// Verify label contains expected text
+Then(
+    /^the "([^"]*)" on the "([0-9]+th|[0-9]+st|[0-9]+nd|[0-9]+rd)" (?:tab|window) should( not)? contain the text "(.*)"$/,
+    async function(
+        this: ScenarioWorld,
+        elementKey: ElementKey,
+        elementPosition: string,
+        negate: boolean,
+        expectedElementText: string
+    ) {
+        const {
+            screen:{ page, context },
+            globalConfig
+        } = this
+
+        console.log(`the ${elementKey} on the ${elementPosition} tab|window should ${negate?'not ':'' }contain the text ${expectedElementText}`)
+        // Regex converts the page number from string to number
+        const pageIndex = Number(elementPosition.match(/\d/g)?.join('')) - 1
+        const elementIdentifier = getElementLocator(page, elementKey, globalConfig)
+
+        await waitFor(async() => {
+            let pages = context.pages()
+            const elementText = await pages[pageIndex].textContent(elementIdentifier)
+            return elementText?.includes(expectedElementText) === !negate
         })
     }
 )
