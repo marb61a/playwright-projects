23b79ea78f67600957de30badeb5143b36451b14
Martin <marb61a@gmail.com>
2022-03-21 17:13:24 +0000

    Updated verify-new-page assertion file with function checking that a new page contains a title

--- TTHub/react-app/e2e/src/step-definitions/assertions/verify-new-page.ts
+++ TTHub/react-app/e2e/src/step-definitions/assertions/verify-new-page.ts
@@ -4,3 +4,29 @@ import { ElementKey } from '../../env/global'
 import { getElementLocator } from '../../support/web-element-helper'
 import {ScenarioWorld} from '../setup/world'
 import { waitFor } from '../../support/wait-for-behaviour'
+
+Then(
+    /^the "([0-9]+th|[0-9]+st|[0-9]+nd|[0-9]+rd)" (?:tab|window) should( not)? contain the title "(.*)"$/,
+    async function(
+        this: ScenarioWorld,
+        elementPosition: string,
+        negate: boolean,
+        expectedTitle: string
+    ) {
+        const {
+            screen: { page, context }
+        } = this
+
+        console.log(`the ${elementPosition} tab should ${negate?'not ':''}contain the title ${expectedTitle}`)
+
+        // Regex converts the page number from string to number
+        const pageIndex = Number(elementPosition.match(/\d/g)?.join('')) - 1
+        await page.waitForTimeout(1000)
+
+        await waitFor(async () => {
+            let pages = context.pages()
+            const pageTitle = await pages[pageIndex].title()
+            return pageTitle?.includes(expectedTitle) === !negate 
+        })
+    }
+)
