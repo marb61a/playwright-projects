c8c9998ad059b8fd8c98c7f8a87a1376f258b9d1
Martin <marb61a@gmail.com>
2022-03-29 16:58:57 +0100

    Added verify-stored-value.ts assertion file

---
+++ TTHub/react-app/e2e/src/step-definitions/assertions/varify-stored-value.ts
@@ -0,0 +1,31 @@
+import { Then } from '@cucumber/cucumber'
+
+import { ElementKey } from '../../env/global'
+import { getElementLocator } from '../../support/web-element-helper'
+import {ScenarioWorld} from '../setup/world'
+import { waitFor } from '../../support/wait-for-behaviour'
+
+Then(
+    /^the "([^"]*)" should( not)? stored in global values$/,
+    async function(
+        this: ScenarioWorld,
+        elementKey: ElementKey,
+        negate: boolean,
+        variableKey: string
+    ) {
+        const {
+            screen: {page},
+            globalConfig,
+            globalVariables
+        } = this
+
+        console.log(`The ${elementKey} should ${negate?'not ':''} equal the ${globalVariables[variableKey]} stored in global variables`)
+        const elementIdentifier = getElementLocator(page, elementKey, globalConfig)
+
+        await waitFor(async() => {
+            const elementText = await page.textContent(elementIdentifier)
+            const variableValue = globalVariables[variableKey]
+            return (elementText === variableValue) === !negate
+        })
+    }
+)
