2db8eed27c498e1674bd0d4e9dd343cd186db84c
Martin <marb61a@gmail.com>
2022-04-06 17:51:42 +0100

    Added getViewport function to browser behaviour to enable using multiple device viewports

--- TTHub/react-app/e2e/src/support/browser-behaviour.ts
+++ TTHub/react-app/e2e/src/support/browser-behaviour.ts
@@ -1,4 +1,23 @@
 import { devices } from "playwright";
 import { envNumber } from "../env/parseEnv";
 
-export const getViewPort = ()
+export const getViewPort = (): {width: number, height: number} => {
+    let viewPort 
+    // If the environment has not set an emulation value then use the browser value
+    const emulation = process.env.EMULATION || "browser"
+
+    if(emulation != "browser") {
+        const device = devices[emulation]
+        viewPort = {
+            width: device.viewport.width,
+            height: device.viewport.height
+        }
+    } else {
+        viewPort = {
+            width: envNumber('BROWSER_WIDTH'),
+            height: envNumber('BROWSER_HEIGHT')
+        }
+    }
+
+    return viewPort
+}
