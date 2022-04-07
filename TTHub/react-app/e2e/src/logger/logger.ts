39eed5c2168826ae67df09e258dfc1d296694fc5
Martin <marb61a@gmail.com>
2022-04-07 16:56:50 +0100

    Adding logging functionality to logger file

--- TTHub/react-app/e2e/src/logger/logger.ts
+++ TTHub/react-app/e2e/src/logger/logger.ts
@@ -16,10 +16,35 @@ type Logger = {
 
 let loggerSingleton: Logger | null = null
 
+const createLogger = (logLevel: LogLevel): Logger => {
+    const activeLogLevels = getLogLevel(logLevel)
+    const logger = logFuncAtLevels(activeLogLevels)
+
+    return LOG_LEVELS.reduce(
+        (accumulator: Record<string, LogFunction>, level: LogLevel) => ({
+            ...accumulator,
+            [level]: (...msg: any[]) => logger(level, ...msg)
+        }),
+        {}
+    )
+}
+
+export const logLevelIsT = <T extends string>(logLevel: string, options: readonly string[]): logLevel is T => {
+    return options.includes(logLevel)
+}
+
+export const stringIsOfOptions = <T extends string> (logLevel: string, options: readonly string[]): T => {
+    if(logLevelIsT(logLevel, options)) {
+        return logLevel as T
+    }
+
+    throw Error(`Logger '${logLevel}' needs to be one of`)
+}
+
 export const getLogger= (): Logger => {
     if(!loggerSingleton) {
         const logLevel = env('LOG_LEVEL')
-        const validLogLevel = stringIsOFOptions<LogLevel>(logLevel, LOG_LEVELS)
+        const validLogLevel = stringIsOfOptions<LogLevel>(logLevel, LOG_LEVELS)
         loggerSingleton = createLogger(validLogLevel)
     }
 
