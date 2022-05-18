0a8063ac4002ce6acdfd5e8f4f24247ddb0434cc
Martin <marb61a@gmail.com>
2022-05-18 16:50:12 +0100

    Updated waitFor functionality in text equal and value contains assertions in verify-element-value assertion file

--- TTHub/react-app/e2e/src/step-definitions/assertions/verify-element-value.ts
+++ TTHub/react-app/e2e/src/step-definitions/assertions/verify-element-value.ts
@@ -64,27 +64,57 @@ Then(
         const elementIdentifier = getElementLocator(page, elementKey, globalConfig)
 
         await waitFor(async() => {
-            const elementText = (await page.textContent(elementIdentifier))
-            // Return will verify whether true or false
-            return (elementText === expectedElementText) === !negate
+            const elementStable = await waitForSelector(page, elementIdentifier)
+
+            if(elementStable) {
+                const elementText = await getElementText(page, elementIdentifier)
+
+                if ((elementText === expectedElementText) === !negate) {
+                    return waitForResult.PASS
+                } else {
+                    return waitForResult.FAIL
+                }
+            } else {
+                return waitForResult.ELEMENT_NOT_AVAILABLE
+            }
+        },
+        globalConfig,
+        {
+            target: elementKey,
+            failureMessage: `Expected ${elementKey} to ${negate ? 'not ' : ''}equal the text ${expectedElementText}`
         })
     }
 )
 
 Then(
     /^the "([^"]*)" should( not)? contain the value "(.*)"$/,
-    async function(this: ScenarioWorld, elementKey: ElementKey, negate: boolean, elementValue: string) {
+    async function(this: ScenarioWorld, elementKey: ElementKey, negate: boolean, expectedElementValue: string) {
         const {
             screen: {page},
             globalConfig
         } = this
 
-        logger.log(`the ${elementKey} should ${negate?'not':''} equal the value ${elementValue}`)
         const elementIdentifier = getElementLocator(page, elementKey, globalConfig)
 
         await waitFor(async() => {
-            const elementAttributes = await getValue(page, elementIdentifier)
-            return elementAttributes?.includes(elementValue) === !negate
+            const elementStable = await waitForSelector(page, elementIdentifier)
+
+            if (elementStable) {
+                const elementAttribute = await getElementValue(page, elementIdentifier)
+
+                if (elementAttribute?.includes(expectedElementValue) === !negate) {
+                    return waitForResult.PASS
+                } else {
+                    return waitForResult.FAIL
+                }
+            } else {
+                return waitForResult.ELEMENT_NOT_AVAILABLE
+            }
+        },
+        globalConfig,
+        {
+            target: elementKey,
+            failureMessage: `Expected ${elementKey} to ${negate ? 'not ' : ''}contain the value ${expectedElementValue}`
         })
     }
 )
