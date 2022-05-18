927747d615baa292c41234a7ead3884e9670b1ff
Martin <marb61a@gmail.com>
2022-05-18 17:06:51 +0100

    Updated waitFor functionality in value equal and enabled assertions in verify-element-value assertion file

--- TTHub/react-app/e2e/src/step-definitions/assertions/verify-element-value.ts
+++ TTHub/react-app/e2e/src/step-definitions/assertions/verify-element-value.ts
@@ -122,18 +122,34 @@ Then(
 // Testing the required text box on the playground page
 Then(
     /^the "([^"]*)" should( not)? equal the value "(.*)"$/,
-    async function(this: ScenarioWorld, elementKey: ElementKey, negate: boolean, elementValue: string) {
+    async function(this: ScenarioWorld, elementKey: ElementKey, negate: boolean, expectedElementValue: string) {
         const {
             screen: {page},
             globalConfig
         } = this
 
-        logger.log(`the ${elementKey} should ${negate?'not':''} equal the value ${elementValue}`)
+        logger.log(`the ${elementKey} should ${negate?'not ':''}equal the value ${expectedElementValue}`)
         const elementIdentifier = getElementLocator(page, elementKey, globalConfig)
 
         await waitFor(async() => {
-            const elementAttribute = await getValue(page, elementIdentifier)
-            return (elementAttribute === elementValue) === !negate 
+            const elementStable = await waitForSelector(page, elementIdentifier)
+
+            if (elementStable) {
+                const elementAttribute = await getElementValue(page, elementIdentifier)
+
+                if ((elementAttribute === expectedElementValue) === !negate) {
+                    return waitForResult.PASS
+                } else {
+                    return waitForResult.FAIL
+                }
+            } else {
+                return waitForResult.ELEMENT_NOT_AVAILABLE
+            }
+        },
+        globalConfig,
+        {
+            target: elementKey,
+            failureMessage: `Expected ${elementKey} to ${negate ? 'not ' : ''}equal the value ${expectedElementValue}`
         })
     }
 )
@@ -150,9 +166,24 @@ Then(
         logger.log(`The ${elementKey} should ${negate?'not':''}be enabled`)
         const elementIdentifier = getElementLocator(page, elementKey, globalConfig)
 
-        await waitFor(async() => {
-            const isElementEnabled = await page.isEnabled(elementIdentifier)
-            return isElementEnabled === !negate
+        await waitFor(async () => {
+            const elementStable = await waitForSelector(page, elementIdentifier)
+
+            if (elementStable) {
+                const isElementEnabled = await elementEnabled(page, elementIdentifier)
+                if (isElementEnabled === !negate) {
+                    return waitForResult.PASS
+                } else {
+                    return waitForResult.FAIL
+                }
+            } else {
+                return waitForResult.ELEMENT_NOT_AVAILABLE
+            }
+        },
+        globalConfig,
+        {
+            target: elementKey,
+            failureMessage: `Expected ${elementKey} should ${negate ? 'not ' : ''}be enabled`
         })
     }
 )
