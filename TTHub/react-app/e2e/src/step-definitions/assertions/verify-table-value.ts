18102c74c302fb2d1d7f4db1979598397dc1bbf7
Martin <marb61a@gmail.com>
2022-05-20 16:57:37 +0100

    Updated waitFor functionality in assertions in verify-table-value assertion file

--- TTHub/react-app/e2e/src/step-definitions/assertions/verify-table-value.ts
+++ TTHub/react-app/e2e/src/step-definitions/assertions/verify-table-value.ts
@@ -3,7 +3,13 @@ import { DataTable, Then } from '@cucumber/cucumber'
 import { ElementKey } from '../../env/global'
 import { getElementLocator } from '../../support/web-element-helper'
 import {ScenarioWorld} from '../setup/world'
-import { waitFor } from '../../support/wait-for-behaviour'
+import {
+    waitFor,
+    waitForResult,
+    waitForSelector
+} from '../../support/wait-for-behaviour'
+import {logger} from "../../logger"
+import { getTableData } from "../../support/html-behaviour"
 
 Then(
     /^the "([^"]*)" table should( not)? equal the following:$/,
@@ -17,17 +23,23 @@ Then(
         const elementIdentifier = getElementLocator(page, elementKey, globalConfig)
 
         await waitFor(async () => {
-            // Retrieve table with table broken into array of strings
-            // It is in the waitFor to avoid errors when trying to fetch data before it is
-            // actually available, it will continue to retry until the maximum limit has been reached
-            const dataBefore = await page.$$eval(elementIdentifier+"tbody tr", (rows) => {
-                return rows.map(row => {
-                    const cells = row.querySelectorAll('td')
-                    return Array.from(cells).map(cell => cell.textContent)
-                })
-            })
-
-            return JSON.stringify(dataBefore) === JSON.stringify(dataTable.raw()) === !negate
+            const elementStable = await waitForSelector(page, elementIdentifier)
+            if (elementStable) {
+                const tableData = await getTableData(page, elementIdentifier)
+                
+                if (tableData === JSON.stringify(dataTable.raw()) === !negate) {
+                    return waitForResult.PASS
+                } else {
+                    return waitForResult.FAIL
+                }
+            } else {
+                return waitForResult.ELEMENT_NOT_AVAILABLE
+            }
+        },
+        globalConfig,
+        {
+            target: elementKey,
+            failureMessage: `Expected ${elementKey} to ${negate ? 'not ' : ''}equal ${dataTable.raw()}`
         })
     }
 )
