89828d8faf6a2b4c85012090c7614339e82247a1
Martin <marb61a@gmail.com>
2022-05-20 16:45:53 +0100

    Updated waitFor functionality in assertions in verify-stored-value assertion file

--- TTHub/react-app/e2e/src/step-definitions/assertions/verify-stored-value.ts
+++ TTHub/react-app/e2e/src/step-definitions/assertions/verify-stored-value.ts
@@ -3,7 +3,13 @@ import { Then } from '@cucumber/cucumber'
 import { ElementKey } from '../../env/global'
 import { getElementLocator } from '../../support/web-element-helper'
 import {ScenarioWorld} from '../setup/world'
-import { waitFor } from '../../support/wait-for-behaviour'
+import {
+    waitFor,
+    waitForResult,
+    waitForSelector
+} from '../../support/wait-for-behaviour'
+import {logger} from "../../logger"
+import { getElementText } from "../../support/html-behaviour"
 
 Then(
     /^the "([^"]*)" should( not)? stored in global values$/,
@@ -23,9 +29,25 @@ Then(
         const elementIdentifier = getElementLocator(page, elementKey, globalConfig)
 
         await waitFor(async() => {
-            const elementText = await page.textContent(elementIdentifier)
+            const elementStable = await waitForSelector(page, elementIdentifier)
             const variableValue = globalVariables[variableKey]
-            return (elementText === variableValue) === !negate
+
+            if (elementStable) {
+                const elementText = await getElementText(page, elementIdentifier)
+
+                if ((elementText === variableValue) === !negate) {
+                    return waitForResult.PASS
+                } else {
+                    return waitForResult.FAIL
+                }
+            } else {
+                return waitForResult.ELEMENT_NOT_AVAILABLE
+            }
+        },
+        globalConfig,
+        {
+            target: elementKey,
+            failureMessage: `Expected ${elementKey} to ${negate ? 'not ' : ''}contain the ${variableKey} in global variables`
         })
     }
 )
@@ -49,9 +71,25 @@ Then(
         const elementIdentifier = getElementLocator(page, elementKey, globalConfig)
 
         await waitFor(async() => {
-            const elementText = await page.textContent(elementIdentifier)
+            const elementStable = await waitForSelector(page, elementIdentifier)
             const variableValue = globalVariables[variableKey]
-            return elementText?.includes(variableValue) === !negate
+
+            if (elementStable) {
+                const elementText = await getElementText(page, elementIdentifier)
+                
+                if (elementText?.includes(variableValue) === !negate) {
+                    return waitForResult.PASS
+                } else {
+                    return waitForResult.FAIL
+                }
+            } else {
+                return waitForResult.ELEMENT_NOT_AVAILABLE
+            }
+        },
+        globalConfig,
+        {
+            target: elementKey,
+            failureMessage: `Expected ${elementKey} to ${negate ? 'not ' : ''}contain the ${variableKey} in global variables`
         }) 
     }
 )
